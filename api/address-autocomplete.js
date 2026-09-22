33050b34c6964de86d7d0e80fe3e720e053f2d28
DennyC3 <37904598+DC-AS@users.noreply.github.com>
2026-09-22 18:52:30 -0400

    Limit address suggestions to Brooklyn and Queens

--- api/address-autocomplete.js
+++ api/address-autocomplete.js
@@ -89,7 +89,7 @@ export default async function handler(req, res) {
           inRange: isNewYork && (isBrooklyn || isQueens),
         };
       })
-      .filter((result) => result.formatted);
+      .filter((result) => result.formatted && result.inRange === true);
 
     return res.status(200).json({
       suggestions,
